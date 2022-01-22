ef24cd89c352d095aa5d084515dbc41ed16e5785
ajib6ept <asAlbert@narod.ru>
2022-01-22 12:36:34 +0300

    add help output

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,8 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    args = parser.parse_args()
