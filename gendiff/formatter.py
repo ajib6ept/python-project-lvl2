c4c6cff2bfb3be9976d436acecf2e9dd6e86c663
ajib6ept <asAlbert@narod.ru>
2022-02-02 07:10:54 +0300

    add linter bug fixes

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,25 +1,29 @@
 import json
 
 
-def _get_value(item, depth, indent=4):
-    if item == False:
+def make_lines_from_dict(dict_item, indent):
+    res = (
+        json.dumps(dict_item, indent=4)
+        .replace('"', "")
+        .replace(",", "")
+        .split("\n")
+    )
+    for k, v in enumerate(res):
+        if v.startswith(" "):
+            res[k] = " " * indent + res[k]
+    res[-1] = " " * indent + res[-1]
+    return "\n".join(res)
+
+
+def _get_value(item, indent=4):
+    if item is False:
         return "false"
-    if item == True:
+    if item is True:
         return "true"
     if item is None:
         return "null"
     elif isinstance(item, dict):
-        res = (
-            json.dumps(item, indent=4)
-            .replace('"', "")
-            .replace(",", "")
-            .split("\n")
-        )
-        for k, v in enumerate(res):
-            if v.startswith(" "):
-                res[k] = " " * indent + res[k]
-        res[-1] = " " * indent + res[-1]
-        return "\n".join(res)
+        return make_lines_from_dict(item, indent)
     return item
 
 
@@ -37,25 +41,24 @@ def make_result(depth, symbol, key, value):
     return f"{indent}{key}: {value}\n"
 
 
-def stylish(files_difference, indent=4, depth=1):
+def stylish(files_difference, indent=4, depth=1):  # noqa: C901
 
     keys = list(files_difference.keys())
     keys.sort()
     result = "{\n"
     for key in keys:
         value = files_difference[key].get("value")
-        value1 = _get_value(files_difference[key].get("value1"), depth, indent)
-        value2 = _get_value(files_difference[key].get("value2"), depth, indent)
+        value1 = _get_value(files_difference[key].get("value1"), indent)
+        value2 = _get_value(files_difference[key].get("value2"), indent)
         if files_difference[key]["status"] == "equal":
             indent1 = make_indent(depth)
             result = result + f"{indent1}{key}: {value1}\n"
         elif files_difference[key]["status"] == "nested":
             indent1 = make_indent(depth)
-            result = (
-                result
-                + indent1
-                + f"{key}: {stylish(value, indent=indent+4, depth=depth+1)}\n"
+            nested_result = (
+                f"{key}: {stylish(value, indent=indent+4, depth=depth+1)}\n"
             )
+            result = result + indent1 + nested_result
         elif files_difference[key]["status"] == "removed":
             indent1 = make_indent(depth, symbol="-")
             result = result + f"{indent1}{key}: {value1}\n"
