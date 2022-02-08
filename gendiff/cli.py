fdb50b22372852ae17370781c1eb17dcdfdd2f78
ajib6ept <asAlbert@narod.ru>
2022-02-08 06:30:15 +0300

    fix the cli function

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,13 +1,14 @@
 import argparse
+
 from gendiff.gendiff import generate_diff
 
 
 def arg_parse():
     parser = argparse.ArgumentParser(description="Generate diff")
     parser.add_argument(
-        "-f", "-format", metavar="FORMAT", help="set format of output"
+        "--f", "--format", metavar="FORMAT", help="set format of output"
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     args = parser.parse_args()
-    print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(args.first_file, args.second_file, format_name=args.f))
