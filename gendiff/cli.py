eef4e5911934441e26e4668cba384e9abc78ccee
ajib6ept <asAlbert@narod.ru>
2022-02-08 06:37:57 +0300

    add the default value to the cli

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,7 +6,11 @@ from gendiff.gendiff import generate_diff
 def arg_parse():
     parser = argparse.ArgumentParser(description="Generate diff")
     parser.add_argument(
-        "--f", "--format", metavar="FORMAT", help="set format of output"
+        "--f",
+        "--format",
+        metavar="FORMAT",
+        default="stylish",
+        help="set format of output",
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
