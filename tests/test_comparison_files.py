158203975a2958c44b7b0e691081a445b57029a7
ajib6ept <asAlbert@narod.ru>
2022-02-09 07:01:11 +0300

    tests refactoring

--- tests/test_comparison_files.py
+++ tests/test_comparison_files.py
@@ -1,12 +1,18 @@
 import json
 
-from fixtures.flat_format_comparison_result import (
-    flat_files_result,
-    nested_files_result,
-)
-from fixtures.flat_json_files_comparison_result import result as flat_result
-from fixtures.nested_files_comparison_result import result as nested_result
 from gendiff.cli import generate_diff
+from tests.fixtures.plain_format_comparison_result import (
+    flat_files_result as plain_flat_result,
+)
+from tests.fixtures.plain_format_comparison_result import (
+    nested_files_result as plain_nested_result,
+)
+from tests.fixtures.stylish_format_comparison_result import (
+    flat_files_result as stylish_flat_result,
+)
+from tests.fixtures.stylish_format_comparison_result import (
+    nested_files_result as stylish_nested_result,
+)
 
 PATH_FLAT_JSON_FILE1 = "./tests/fixtures/flat_file1.json"
 PATH_FLAT_JSON_FILE2 = "./tests/fixtures/flat_file2.json"
@@ -21,43 +27,66 @@ PATH_NESTED_YAML_FILE1 = "./tests/fixtures/nested_file1.yml"
 PATH_NESTED_YAML_FILE2 = "./tests/fixtures/nested_file2.yml"
 
 
-def test_comparion_flat_json_files():
+def test_default_comparison_flat_json_files():
     assert (
-        generate_diff(PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2) == flat_result
+        generate_diff(PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2)
+        == stylish_flat_result
     )
 
 
-def test_comparion_flat_yaml_files():
+def test_default_comparison_flat_yaml_files():
     assert (
-        generate_diff(PATH_FLAT_YAML_FILE1, PATH_FLAT_YAML_FILE2) == flat_result
+        generate_diff(PATH_FLAT_YAML_FILE1, PATH_FLAT_YAML_FILE2)
+        == stylish_flat_result
     )
 
 
-def test_comparion_nested_json_files():
+def test_default_comparion_nested_json_files():
     assert (
         generate_diff(PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2)
-        == nested_result
+        == stylish_nested_result
     )
 
 
-def test_comparion_nested_yaml_files():
+def test_default_comparion_nested_yaml_files():
     assert (
         generate_diff(PATH_NESTED_YAML_FILE1, PATH_NESTED_YAML_FILE2)
-        == nested_result
+        == stylish_nested_result
     )
 
 
-def test_comparion_flat_json_files_stylish_flat():
+def test_stylish_comparison_flat_json_files():
+    assert (
+        generate_diff(PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "stylish")
+        == stylish_flat_result
+    )
+
+
+def test_plain_comparison_flat_json_files():
     assert (
         generate_diff(PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "plain")
-        == flat_files_result
+        == plain_flat_result
     )
 
 
-def test_comparion_nested_json_files_stylish_flat():
+def test_plain_comparison_flat_yml_files():
+    assert (
+        generate_diff(PATH_FLAT_YAML_FILE1, PATH_FLAT_YAML_FILE2, "plain")
+        == plain_flat_result
+    )
+
+
+def test_plain_comparison_nested_json_files():
     assert (
         generate_diff(PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2, "plain")
-        == nested_files_result
+        == plain_nested_result
+    )
+
+
+def test_plain_comparison_nested_yml_files():
+    assert (
+        generate_diff(PATH_NESTED_YAML_FILE1, PATH_NESTED_YAML_FILE2, "plain")
+        == plain_nested_result
     )
 
 
