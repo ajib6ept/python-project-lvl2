4287cf8df80982ba12ddfd7392715f64f6f47e9c
ajib6ept <asAlbert@narod.ru>
2022-05-05 13:49:34 +0300

    refactoring

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -27,36 +27,24 @@ def diff(file1, file2):
     diff_dict = {}
     keys = list(set(list(file1.keys()) + list(file2.keys())))
     for key in keys:
-        if file1.get(key) == file2.get(key):
-            diff_dict[key] = {
-                "status": "equal",
-                "value1": file1.get(key),
-                "value2": file2.get(key),
-            }
+        file1_value = file1.get(key)
+        file2_value = file2.get(key)
+        diff_dict[key] = {
+            "value1": file1_value,
+            "value2": file2_value,
+        }
+        if file1_value == file2_value:
+            diff_dict[key]["status"] = "equal"
         else:
-            if isinstance(file1.get(key), dict) and isinstance(
-                file2.get(key), dict
-            ):
+            if isinstance(file1_value, dict) and isinstance(file2_value, dict):
                 diff_dict[key] = {
                     "status": "nested",
-                    "value": diff(file1.get(key), file2.get(key)),
+                    "value": diff(file1_value, file2_value),
                 }
             elif key in file1.keys() and key not in file2.keys():
-                diff_dict[str(key)] = {
-                    "status": "removed",
-                    "value1": file1.get(key),
-                    "value2": file2.get(key),
-                }
+                diff_dict[key]["status"] = "removed"
             elif key in file2.keys() and key not in file1.keys():
-                diff_dict[str(key)] = {
-                    "status": "added",
-                    "value1": file1.get(key),
-                    "value2": file2.get(key),
-                }
+                diff_dict[key]["status"] = "added"
             else:
-                diff_dict[str(key)] = {
-                    "status": "changed",
-                    "value1": file1.get(key),
-                    "value2": file2.get(key),
-                }
+                diff_dict[key]["status"] = "changed"
     return diff_dict
