1ec6b77e2b78c92cf162893cd7ffe73b2b67ee6b
ajib6ept <asalbert@narod.ru>
2022-08-02 20:44:16 +0300

    refactoring test

--- tests/test_comparison_files.py
+++ tests/test_comparison_files.py
@@ -1,18 +1,10 @@
 import json
+import os
 
+import pytest
 from gendiff.cli import generate_diff
-from tests.fixtures.plain_format_comparison_result import (
-    flat_files_result as plain_flat_result,
-)
-from tests.fixtures.plain_format_comparison_result import (
-    nested_files_result as plain_nested_result,
-)
-from tests.fixtures.stylish_format_comparison_result import (
-    flat_files_result as stylish_flat_result,
-)
-from tests.fixtures.stylish_format_comparison_result import (
-    nested_files_result as stylish_nested_result,
-)
+
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "fixtures")
 
 PATH_FLAT_JSON_FILE1 = "./tests/fixtures/flat_file1.json"
 PATH_FLAT_JSON_FILE2 = "./tests/fixtures/flat_file2.json"
@@ -27,76 +19,78 @@ PATH_NESTED_YAML_FILE1 = "./tests/fixtures/nested_file1.yml"
 PATH_NESTED_YAML_FILE2 = "./tests/fixtures/nested_file2.yml"
 
 
-def test_default_comparison_flat_json_files():
-    assert (
-        generate_diff(PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2)
-        == stylish_flat_result
-    )
+DEFAULT_COMPARISON_PARAMETRS = [
+    (
+        (PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2),
+        "expected_comparison_flat_files_stylish.txt",
+    ),
+    (
+        (PATH_FLAT_YAML_FILE1, PATH_FLAT_YAML_FILE2),
+        "expected_comparison_flat_files_stylish.txt",
+    ),
+    (
+        (PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2),
+        "expected_comparison_nested_files_stylish.txt",
+    ),
+    (
+        (PATH_NESTED_YAML_FILE1, PATH_NESTED_YAML_FILE2),
+        "expected_comparison_nested_files_stylish.txt",
+    ),
+]
+
+COMPARISON_PARAMETRS_WITH_FORMAT = [
+    (
+        (PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "stylish"),
+        "expected_comparison_flat_files_stylish.txt",
+    ),
+    (
+        (PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "plain"),
+        "expected_comparison_flat_files_plain.txt",
+    ),
+    (
+        (PATH_FLAT_YAML_FILE1, PATH_FLAT_YAML_FILE2, "plain"),
+        "expected_comparison_flat_files_plain.txt",
+    ),
+    (
+        (PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2, "plain"),
+        "expected_comparison_nested_files_plain.txt",
+    ),
+    (
+        (PATH_NESTED_YAML_FILE1, PATH_NESTED_YAML_FILE2, "plain"),
+        "expected_comparison_nested_files_plain.txt",
+    ),
+    (
+        (PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "json"),
+        "expected_comparison_flat_files_json.txt",
+    ),
+    (
+        (PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2, "json"),
+        "expected_comparison_nested_files_json.txt",
+    ),
+]
+
+CHECK_VALID_JSON_PARAMETRS = [
+    (PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "json"),
+    (PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2, "json"),
+]
 
 
-def test_default_comparison_flat_yaml_files():
-    assert (
-        generate_diff(PATH_FLAT_YAML_FILE1, PATH_FLAT_YAML_FILE2)
-        == stylish_flat_result
-    )
+@pytest.mark.parametrize("parametrs", DEFAULT_COMPARISON_PARAMETRS)
+def test_default_comparison_files(parametrs):
+    expected = open(os.path.join(TEST_DATA_DIR, parametrs[1])).read()
+    result = generate_diff(*parametrs[0])
+    assert result == expected
 
 
-def test_default_comparion_nested_json_files():
-    assert (
-        generate_diff(PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2)
-        == stylish_nested_result
-    )
+@pytest.mark.parametrize("parametrs", COMPARISON_PARAMETRS_WITH_FORMAT)
+def test_comparison_files_with_format(parametrs):
+    expected = open(os.path.join(TEST_DATA_DIR, parametrs[1])).read()
+    result = generate_diff(*parametrs[0])
+    assert result == expected
 
 
-def test_default_comparion_nested_yaml_files():
-    assert (
-        generate_diff(PATH_NESTED_YAML_FILE1, PATH_NESTED_YAML_FILE2)
-        == stylish_nested_result
-    )
-
-
-def test_stylish_comparison_flat_json_files():
-    assert (
-        generate_diff(PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "stylish")
-        == stylish_flat_result
-    )
-
-
-def test_plain_comparison_flat_json_files():
-    assert (
-        generate_diff(PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "plain")
-        == plain_flat_result
-    )
-
-
-def test_plain_comparison_flat_yml_files():
-    assert (
-        generate_diff(PATH_FLAT_YAML_FILE1, PATH_FLAT_YAML_FILE2, "plain")
-        == plain_flat_result
-    )
-
-
-def test_plain_comparison_nested_json_files():
-    assert (
-        generate_diff(PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2, "plain")
-        == plain_nested_result
-    )
-
-
-def test_plain_comparison_nested_yml_files():
-    assert (
-        generate_diff(PATH_NESTED_YAML_FILE1, PATH_NESTED_YAML_FILE2, "plain")
-        == plain_nested_result
-    )
-
-
-def test_comparion_flat_json_files_stylish_json():
+@pytest.mark.parametrize("parametrs", CHECK_VALID_JSON_PARAMETRS)
+def test_valid_json(parametrs):
     json.loads(
         generate_diff(PATH_FLAT_JSON_FILE1, PATH_FLAT_JSON_FILE2, "json")
-    )
-
-
-def test_comparion_nested_json_files_stylish_json():
-    json.loads(
-        generate_diff(PATH_NESTED_JSON_FILE1, PATH_NESTED_JSON_FILE2, "json")
     )
